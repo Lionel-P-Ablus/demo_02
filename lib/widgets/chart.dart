10bf99eeee82b62cd0282bd43a6260569b3d542d
Praneed.K <>
2021-08-10 13:34:37 +0700

    populating the chart with values

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:demo_02/models/transaction.dart';
+import 'package:demo_02/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -23,9 +24,15 @@ class Chart extends StatelessWidget {
       }
 
       return {
-        'day': DateFormat.E().format(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
     });
   }
 
@@ -35,7 +42,14 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactionValues.map((data) {
+          // return Text('${data['day']}: ${data['amount']}');
+          return ChartBar(
+            data['day'] as String,
+            data['amount'] as double,
+            totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending,
+          );
+        }).toList(),
       ),
     );
   }
