cf5d1c0ea4aa08afc2da06403e2cdd199ce85cb5
Praneed.K <>
2021-08-11 11:00:01 +0700

    using the device size in conditions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,75 +13,75 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return transactions.isEmpty
         ? LayoutBuilder(builder: (ctx, constraints) {
-      return Column(
-        children: [
-          Text(
-            'No transactions added yet!',
-            style: Theme
-                .of(context)
-                .textTheme
-                .title,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            height: constraints.maxHeight * 0.6,
-            child: Image.asset(
-              'assets/images/waiting.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-        ],
-      );
-    }) : ListView.builder(
-      itemBuilder: (context, index) {
-        return Card(
-          elevation: 5,
-          margin: EdgeInsets.symmetric(
-            vertical: 8,
-            horizontal: 5,
-          ),
-          child: ListTile(
-            leading: Container(
-              height: 60,
-              width: 60,
-              decoration: BoxDecoration(
-                color: Theme
-                    .of(context)
-                    .primaryColor,
-                shape: BoxShape.circle,
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(6),
-                child: FittedBox(
-                  child: Text('\$${transactions[index].amount}'),
+            return Column(
+              children: [
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.title,
                 ),
-              ),
-            ),
-            title: Text(
-              transactions[index].title,
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .title,
-            ),
-            subtitle: Text(
-              DateFormat.yMMMd().format(transactions[index].date),
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              color: Theme
-                  .of(context)
-                  .errorColor,
-              onPressed: () {
-                deleteTx(transactions[index].id);
-              },
-            ),
-          ),
-        );
-      },
-      itemCount: transactions.length,
-    );
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            );
+          })
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text('\$${transactions[index].amount}'),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? FlatButton.icon(
+                          icon: Icon(Icons.delete),
+                          label: Text('Delete'),
+                          textColor: Theme.of(context).errorColor,
+                          onPressed: () {
+                            deleteTx(transactions[index].id);
+                          },
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () {
+                            deleteTx(transactions[index].id);
+                          },
+                        ),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
